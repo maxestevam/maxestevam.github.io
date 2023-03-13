89ed0eeaeed0c00abeb5c7663e24bb5c7675ba4f
Max Estevam <32106654+maxestevam@users.noreply.github.com>
2023-03-13 09:54:38 -0300

    Update made-by.js

--- js/made-by.js
+++ js/made-by.js
@@ -2,7 +2,7 @@ let styles = `.made-by a{filter: grayscale(1);}`
 var styleSheet = document.createElement("style")
 styleSheet.innerText = styles
 document.head.appendChild(styleSheet)
-let website = `https://${window.hostname}`
+let website = `https://${location.host}`
 let whatsLink = `https://wa.me/5543991108116?&text=Olá, vim por referência do site ${website}`
 
 let madeBy = document.querySelector('.made-by')
