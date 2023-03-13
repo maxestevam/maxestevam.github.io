e331ccf01e304ecb588e9707ec1c55749977e41f
Max Estevam <32106654+maxestevam@users.noreply.github.com>
2023-03-13 09:39:18 -0300

    Update made-by.js

--- js/made-by.js
+++ js/made-by.js
@@ -1,2 +1,7 @@
+let styleSheet = `.made-by a{filter: grayscale(1);}`
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+
 let madeBy = document.querySelector('.made-by')
 madeBy.innerHTML = 'Made By <a href="#">Max Estevam</a>'
