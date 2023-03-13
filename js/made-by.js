4830d3bde46e7b3d53a9e46e842c1db6dc2d6569
Max Estevam <32106654+maxestevam@users.noreply.github.com>
2023-03-13 09:48:52 -0300

    Update made-by.js

--- js/made-by.js
+++ js/made-by.js
@@ -2,6 +2,8 @@ let styles = `.made-by a{filter: grayscale(1);}`
 var styleSheet = document.createElement("style")
 styleSheet.innerText = styles
 document.head.appendChild(styleSheet)
+let website = `https://${window.hostname}`
+let whats = `https://wa.me/5543991108116?&text=Olá, vim por referência do site ${website}`
 
 let madeBy = document.querySelector('.made-by')
 madeBy.innerHTML = 'Made By <a href="#">Max Estevam</a>'
